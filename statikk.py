962f4efb6f822d6aa2a21a866bd06e3abf471f9d
akselell <aksel.e.johannessen@gmail.com>
2019-09-03 18:47:37 +0200

    first commit

---
+++ statikk.py
@@ -0,0 +1,15 @@
+import numpy as np
+import math
+
+matrisen = np.linalg.inv([[1, 3, 3],
+       [1, 4, 3],
+       [1, 3, 4]])
+
+matrise2 = np.linalg.inv([[97280, -23040], [-23040, 30720]])
+
+
+#for node in nodes:
+
+
+
+print(matrise2)
