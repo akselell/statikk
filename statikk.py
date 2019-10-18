3d30a5c2d344ed096646d4b18130fd15929e85cf
akselell <aksel.e.johannessen@gmail.com>
2019-10-18 14:07:55 +0200

    lagt til stivhetsmatriser i globalt system

--- statikk.py
+++ statikk.py
@@ -1,7 +1,6 @@
 import math
-import numpy as np
 import re
-import math
+import numpy as np
 
 matrisen = np.linalg.inv([[1, 3, 3],
                           [1, 4, 3],
@@ -35,6 +34,10 @@ class Truss:
         self.E = E
         self.A = A
         self.length = self.get_length()
+        self.c = self.get_c()
+        self.s = self.get_s()
+        self.matrix= self.get_local_matrix()
+
 
     def get_length(self):
         x1 = self.start[0]
@@ -45,8 +48,27 @@ class Truss:
         y = y2 - y1
         return math.sqrt(x**2 + y**2)
 
+    def get_c(self):
+        x1 = self.start[0]
+        x2 = self.end[0]
+        return (x2-x1)/self.get_length()
+
+    def get_s(self):    
+        y1 = self.start[1]
+        y2 = self.end[1]
+        return (y2-y1)/self.get_length()
+    
+    def get_local_matrix(self):
+        k = np.array([[(self.E*self.A)/self.length, -(self.E*self.A)/self.length],
+             [-(self.E*self.A)/self.length, (self.E*self.A)/self.length]])
+        t = np.array([[self.c, self.s, 0, 0],
+             [0, 0, self.c, self.s]])
+        tt = t.transpose()
+        a = np.dot(tt, k)
+        return np.dot(a, t)
+
     def __repr__(self):
-        return f"\nstart:{self.start}, end:{self.end}"
+        return f"\nstart:{self.start}, end:{self.end} matrix:{self.matrix}" 
 
 #for node in nodes:
 
@@ -58,7 +80,6 @@ with open("example1.txt") as f:
             pass
         else:
             trusses.append(Truss(line))
-
 
 
 print(trusses)
