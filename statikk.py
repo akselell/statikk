21f1ead000f1f9fadc89624a8130fae610ec9c79
akselell <aksel.e.johannessen@gmail.com>
2019-10-18 18:38:59 +0200

    endret til bjelke matrise

--- statikk.py
+++ statikk.py
@@ -2,15 +2,15 @@ import math
 import re
 import numpy as np
 
-matrisen = np.linalg.inv([[1, 3, 3],
-                          [1, 4, 3],
-                          [1, 3, 4]])
-
-matrise2 = np.linalg.inv([[97280, -23040], [-23040, 30720]])
+#matrisen = np.linalg.inv([[1, 3, 3],
+#                          [1, 4, 3],
+#                          [1, 3, 4]])
+#
+#matrise2 = np.linalg.inv([[97280, -23040], [-23040, 30720]])
 
 class Truss:
     """
-    a truss can take loads
+    A truss can take loads
     """
     def __init__(self, line):
         print(repr(line))
@@ -22,21 +22,43 @@ class Truss:
             r", "
             r"(\d+e-?\d+)"
             r", "
+            r"(\d+e-?\d+)"
+            r", "
+            r"(\d+e-?\d+)"
+            r", "
+            r"(\d+e-?\d+)"
+            r", "
+            r"(\d+e-?\d+)"
+            r", "
+            r"(\d+e-?\d+)"
+            r", "
+            r"(\d+e-?\d+)"
+            r", "
+            r"(\d+e-?\d+)"
+            r", "
             r"(\d+e-?\d+)", line)
-        start1, start2, end1, end2, E, A = match.groups()
+        start1, start2, end1, end2, E, A, I, u1, u2, u3, u4, u5, u6 = match.groups()
         start = (float(start1), float(start2))
         end = (float(end1), float(end2))
         E = float(E)
         A = float(A)
+        I = float(I) * 0
 
         self.start = start
         self.end = end
         self.E = E
         self.A = A
+        self.I = I
+        self.u1 = u1
+        self.u2 = u2
+        self.u3 = u3
+        self.u4 = u4
+        self.u5 = u5
+        self.u6 = u6
         self.length = self.get_length()
         self.c = self.get_c()
         self.s = self.get_s()
-        self.matrix= self.get_local_matrix()
+        self.matrix = self.get_local_matrix()
 
 
     def get_length(self):
@@ -57,18 +79,37 @@ class Truss:
         y1 = self.start[1]
         y2 = self.end[1]
         return (y2-y1)/self.get_length()
-    
-    def get_local_matrix(self):
+
+    def get_local_matrix2(self):
         k = np.array([[(self.E*self.A)/self.length, -(self.E*self.A)/self.length],
-             [-(self.E*self.A)/self.length, (self.E*self.A)/self.length]])
+                      [-(self.E*self.A)/self.length, (self.E*self.A)/self.length]])
         t = np.array([[self.c, self.s, 0, 0],
-             [0, 0, self.c, self.s]])
+                      [0, 0, self.c, self.s]])
+        tt = t.transpose()
+        a = np.dot(tt, k)
+        return np.dot(a, t)
+
+    def get_local_matrix(self):
+        k = np.array([
+                      [(self.E*self.A)/self.length, 0, 0, -(self.E*self.A)/self.length, 0, 0],
+                      [0, (12*self.E*self.I)/self.length**3, (6*self.E*self.I)/self.length**2, 0, -(12*self.E*self.I)/self.length**3, (6*self.E*self.I)/self.length**2],
+                      [0, (6*self.E*self.I)/self.length**2, (4*self.E*self.I)/self.length, 0, -(6*self.E*self.I)/self.length**2, (2*self.E*self.I)/self.length],
+                      [-(self.E*self.A)/self.length, 0, 0, (self.E*self.A)/self.length, 0, 0],
+                      [0, -(12*self.E*self.I)/self.length**3, -(6*self.E*self.I)/self.length**2, 0, (12*self.E*self.I)/self.length**3, -(6*self.E*self.I)/self.length**2],
+                      [0, (6*self.E*self.I)/self.length**2, (2*self.E*self.I)/self.length, 0, -(6*self.E*self.I)/self.length**2, (4*self.E*self.I)/self.length]
+                      ])
+        t = np.array([[self.c, self.s, 0, 0, 0, 0],
+                      [-self.s, self.c, 0, 0, 0, 0],
+                      [0, 0, 1, 0, 0, 0],
+                      [0, 0, 0, self.c, self.s, 0],
+                      [0, 0, 0, -self.s, self.c, 0],
+                      [0, 0, 0, 0, 0, 1]])
         tt = t.transpose()
         a = np.dot(tt, k)
         return np.dot(a, t)
 
     def __repr__(self):
-        return f"\nstart:{self.start}, end:{self.end} matrix:{self.matrix}" 
+        return f"\nstart:{self.start}, end:{self.end} matrix:\n{self.matrix}" 
 
 #for node in nodes:
 
@@ -80,6 +121,7 @@ with open("example1.txt") as f:
             pass
         else:
             trusses.append(Truss(line))
+
 
 
 print(trusses)
